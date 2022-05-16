063fe3fc58965875978aa897f357017108f082f1
MaureenEk <mekweribe28@gmail.com>
2022-05-16 04:39:17 -0400

    JS

---
+++ dom.js
@@ -0,0 +1,18 @@
+const para = document.createElement("p")
+const node = document.createTextNode("My name is your Maureen")
+para. appendChild(node);
+const element = document. querySelector("intro");
+
+let favoriteBooks = [
+    "things fall apart",
+    "hold my hand",
+    "unquestionable",
+    "its my life",
+    "becoming me",
+    "worrisome",
+    "one",
+    "everytime",
+    "just us",
+    "introvert"
+]
+
